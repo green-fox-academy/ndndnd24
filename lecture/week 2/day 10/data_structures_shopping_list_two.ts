62fa9b00381ed5f261d1e59dd682c796b92c4073
Németh Dávid <ndndnd24@gmail.com>
2019-07-05 17:57:11 +0200

    day 10 update 2

--- lecture/week 2/day 10/data_structures_shopping_list_two.ts
+++ lecture/week 2/day 10/data_structures_shopping_list_two.ts
@@ -38,6 +38,125 @@ Who buys more Potato?
 Who buys more different products?
 Who buys more products? (piece)*/
 
+function bobPay(prices, bob, alice) {
+    let sum: number = 0;
+    let price: number = 0;
+    for (let i: number = 0; i < Object.keys(bob).length; i++) {
+        let printKeyPrice: any[] = Object.keys(prices);
+        let printKeyBob: any[] = Object.keys(bob);
+        let searchedKeyBob: any = printKeyBob[i];
+        for (let indexThree: number = 0; indexThree < Object.keys(prices).length; indexThree++) {
+            if (searchedKeyBob == printKeyPrice[indexThree]) {
+                price = prices[printKeyPrice[indexThree]];
+                sum = sum + price * bob[printKeyPrice[indexThree]];
+            }
+        }
+    }
+    return sum;
+}
 
+function alicePay(prices, bob, alice) {
+    let sum: number = 0;
+    let price: number = 0;
+    for (let i: number = 0; i < Object.keys(alice).length; i++) {
+        let printKeyPrice: any[] = Object.keys(prices);
+        let printKeyBob: any[] = Object.keys(alice);
+        let searchedKeyBob: any = printKeyBob[i];
+        for (let indexThree: number = 0; indexThree < Object.keys(prices).length; indexThree++) {
+            if (searchedKeyBob == printKeyPrice[indexThree]) {
+                price = prices[printKeyPrice[indexThree]];
+                sum = sum + price * alice[printKeyPrice[indexThree]];
+            }
+        }
+    }
+    return sum;
+}
 
+function rice(prices, bob, alice) {
+    let printKeyBob: any = Object.keys(bob);
+    let searchedKeyBob: any = "Rice";
+    let riceBob: number = 0;
+    let riceAlice: number = 0;
+    for (let indexThree: number = 0; indexThree < Object.keys(bob).length; indexThree++) {
+        if (searchedKeyBob == printKeyBob[indexThree]) {
+            riceBob = bob[printKeyBob[indexThree]];
+        }
+    }
+    let printKeyAlice: any = Object.keys(alice);
+    let searchedKeyAlice: any = "Rice";
+    for (let indexThree: number = 0; indexThree < Object.keys(alice).length; indexThree++) {
+        if (searchedKeyAlice == printKeyAlice[indexThree]) {
+            riceAlice = alice[printKeyAlice[indexThree]];
+        }
+    }
+    if (riceBob > riceAlice) {
+        return 'Bob';
+    } else {
+        return 'Alice';
+    }
+}
 
+function potato(prices, bob, alice) {
+    let printKeyBob: any = Object.keys(bob);
+    let searchedKeyBob: any = "Potato";
+    let potatoBob: number = 0;
+    let potatoAlice: number = 0;
+    for (let indexThree: number = 0; indexThree < Object.keys(bob).length; indexThree++) {
+        if (searchedKeyBob == printKeyBob[indexThree]) {
+            potatoBob = bob[printKeyBob[indexThree]];
+        }
+    }
+    let printKeyAlice: any = Object.keys(alice);
+    let searchedKeyAlice: any = "Potato";
+    for (let indexThree: number = 0; indexThree < Object.keys(alice).length; indexThree++) {
+        if (searchedKeyAlice == printKeyAlice[indexThree]) {
+            potatoAlice = alice[printKeyAlice[indexThree]];
+        }
+    }
+    if (potatoBob > potatoAlice) {
+        return 'Bob';
+    } else {
+        return 'Alice';
+    }
+}
+
+function different(prices, bob, alice) {
+    if (Object.keys(bob).length > Object.keys(alice).length) {
+        return 'Bob';
+    } else {
+        return 'Alice';
+    }
+}
+
+function moreProducts(prices, bob, alice) {
+    let keyValuesBob: any[] = Object.keys(bob);
+    let sumBob: number = 0;
+    let printValuesBob: any[] = [];
+    for (let index: number = 0; index < Object.keys(bob).length; index++) {
+        printValuesBob[index] = bob[keyValuesBob[index]];
+        sumBob = sumBob + printValuesBob[index];
+    }
+    let keyValuesAlice: any[] = Object.keys(alice);
+    let sumAlice: number = 0;
+    let printValuesAlice: any[] = [];
+    for (let index: number = 0; index < Object.keys(alice).length; index++) {
+        printValuesAlice[index] = alice[keyValuesAlice[index]];
+        sumAlice = sumAlice + printValuesAlice[index];
+    }
+    if (sumAlice > sumBob) {
+        return 'Alice';
+    } else {
+        return 'Bob';
+    }
+}
+
+function daApplication(prices, bob, alice) {
+    console.log('Bob pays $' + bobPay(prices, bob, alice));
+    console.log('Alice pays $' + alicePay(prices, bob, alice));
+    console.log(rice(prices, bob, alice) + ' buys more rice');
+    console.log(potato(prices, bob, alice) + ' buys more potato');
+    console.log(different(prices, bob, alice) + ' buys more different types of products');
+    console.log(moreProducts(prices, bob, alice) + ' buys more pieces of products');
+}
+
+daApplication(prices, bob, alice);
