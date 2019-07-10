9ed21f417797bebbc8db79535d0ed1aa787e0d05
Németh Dávid <ndndnd24@gmail.com>
2019-07-10 17:37:01 +0200

    pokemon

---
+++ lecture/week3/day3/pokemon_main.ts
@@ -0,0 +1,30 @@
+import Pokemon from './pokemon'
+
+let pokemonOfAsh: Pokemon[] = initializePokemon();
+
+// Every pokemon has a name and a type.
+// Certain types are effective against others, e.g. water is effective against fire.
+
+// Ash has a few pokemon.
+// A wild pokemon appeared!
+
+let wildPokemon: Pokemon = new Pokemon('Oddish', 'leaf', 'water');
+
+// Which pokemon should Ash use?
+
+
+for (let i: number = 0; i < 5; i++) {
+    if (wildPokemon.type === pokemonOfAsh[i].effectiveAgainst) {
+        console.log('I choose you, ' + pokemonOfAsh[i].name + '!');
+    }
+}
+
+function initializePokemon(): Pokemon[] {
+    return [
+        new Pokemon('Balbasaur', 'leaf', 'water'),
+        new Pokemon('Pikatchu', 'electric', 'water'),
+        new Pokemon('Charizard', 'fire', 'leaf'),
+        new Pokemon('Balbasaur', 'water', 'fire'),
+        new Pokemon('Kingler', 'water', 'fire')
+    ];
+}
