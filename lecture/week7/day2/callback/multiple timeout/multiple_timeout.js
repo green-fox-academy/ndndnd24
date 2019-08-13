899430473f2984dae3f996520a8b61e840e7871f
Németh Dávid <ndndnd24@gmail.com>
2019-08-13 15:39:52 +0200

    multiple timeout, timeout works in the callback function

--- lecture/week7/day2/callback/multiple timeout/multiple_timeout.js
+++ lecture/week7/day2/callback/multiple timeout/multiple_timeout.js
@@ -10,24 +10,26 @@
 
 
 
-setTimeout(() => {
-    console.log('grapes'); // prints after one second
-}, 5000);
-setTimeout(() => {
-    console.log('melon'); // prints after one second
-}, 3000);
-setTimeout(() => {
-    console.log('pear'); // prints after one second
-}, 1000);
-console.log('apple'); // prints first
+// setTimeout(() => {
+//     console.log('grapes'); // prints after one second
+// }, 5000);
+// setTimeout(() => {
+//     console.log('melon'); // prints after one second
+// }, 3000);
+// setTimeout(() => {
+//     console.log('pear'); // prints after one second
+// }, 1000);
+// console.log('apple'); // prints first
 
-
-
+let arrayOfFruits = ['apple', 'pear', 'melon', 'grapes'];
 
 function logStuff(callback, array) {
     for (let i = 0; i < array.length; i++) {
         setTimeout(() => {
             callback(array[i]);
         }, i * 1000 + ((i - 1) * 1000));
+        callback(i * 1000 + ((i - 1) * 1000))
     }
 }
+
+logStuff(console.log, arrayOfFruits);
