d90a3ce2f3b0c8088cc74ca7c0de36d6213b01af
Németh Dávid <ndndnd24@gmail.com>
2019-08-14 12:49:52 +0200

    frontend, greeter working

--- lecture/week7/day3/frontend/app.js
+++ lecture/week7/day3/frontend/app.js
@@ -29,4 +29,28 @@ app.get('/doubling', (req, res) => {
     res.send(doubling);
 });
 
+app.get('/greeter', (req, res) => {
+    let greeter = {};
+    if (req.query.name && req.query.title) {
+        greeter = {
+            welcome_message: `Oh, hi there ${req.query.name}, my dear ${req.query.title}!`
+        };
+    } else {
+        if (req.query.name) {
+            greeter = {
+                error: "Please provide a title!"
+            }
+        } else if (req.query.title) {
+            greeter = {
+                error: "Please provide a name!"
+            };
+        } else {
+            greeter = {
+                error: "Please provide a name and a title!"
+            }
+        }
+    }
+    res.send(greeter);
+})
+
 app.listen(PORT, () => console.log(`App is listening on ${PORT}`));
