e5e56340feaeba4395e669da3befbc5e7129d648
Németh Dávid <ndndnd24@gmail.com>
2019-08-14 12:40:23 +0200

    frontend, doubling working

--- lecture/week7/day3/frontend/app.js
+++ lecture/week7/day3/frontend/app.js
@@ -14,4 +14,19 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.get('/doubling', (req, res) => {
+    let doubling = {}
+    if (req.query.input) {
+        doubling = {
+            received: req.query.input,
+            result: (req.query.input * 2)
+        };
+    } else {
+        doubling = {
+            error: "Please provide an input!"
+        };
+    }
+    res.send(doubling);
+});
+
 app.listen(PORT, () => console.log(`App is listening on ${PORT}`));
