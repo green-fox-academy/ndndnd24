c2c8c8444e4912ffd10c1cd7bce71f74c94f298f
Németh Dávid <ndndnd24@gmail.com>
2019-08-16 15:08:28 +0200

    database added

--- lecture/week7/day5/reddit/reddit_main.js
+++ lecture/week7/day5/reddit/reddit_main.js
@@ -3,7 +3,31 @@
 const express = require('express');
 
 const app = express();
-const PORT = 3002;
+const PORT = 3009;
+
+const mysql = require('mysql');
+
+let conn = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "almafa",
+    database: "reddit"
+});
+
+conn.connect(function (err) {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log('Connection established');
+});
+
+conn.end();
+
+app.get('/hello', (req, res) => {
+    // console.log('Hello world');
+    res.send('Hello world');
+});
 
 app.use(express.json());
 
